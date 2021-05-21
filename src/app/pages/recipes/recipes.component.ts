52f208b7e41bfe4587760b14ddba52457caa5a9d
victoriavermo <victoriavermo@gmail.com>
2021-05-21 12:50:54 +0200

    Show data from local JSON files.

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import rice from '../../../assets/data/rice.json'
+import pasta from '../../../assets/data/pasta.json';
+import fish from '../../../assets/data/fish.json';
+import meat from '../../../assets/data/meat.json';
+import gluten from '../../../assets/data/glutenFree.json';
+import vegan from '../../../assets/data/vegan.json';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+
+  recipes = rice;
+  recipes2 = pasta;
+  recipes3 = fish;
+  recipes4 = gluten;
+  recipes5 = meat;
+  recipes6 = vegan;
 
   constructor() { }
 
