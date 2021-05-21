dd09361e9c2869ed243b6a892d54020efc5c7ea8
Darsruiz <darsruiz@gmail.com>
2021-05-21 14:23:26 +0200

    Shopping list

--- src/app/pages/shopping-list/shopping-list.component.ts
+++ src/app/pages/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import rice from '../../../assets/data/rice.json'
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,9 +9,28 @@ import { Router } from '@angular/router';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ownedIngredients = ['pan','queso','azúcar','cafe'];
+  missingIngredients = ['carne','pollo','pescado','chocolate'];
   constructor(private router: Router) { }
 
+
+  /*
+  select recipe and that recipe tells me the position in the array and then i run a ng for for the elements of that recipe */
   ngOnInit(): void {
+    console.log(rice[0].ingredients[0].name
+      );
   }
 
+  addIngredient(missingIngredient) {
+    /* console.log('addIngredient works', missingIngredient); */
+    this.missingIngredients.push(missingIngredient)
+    this.ownedIngredients = this.ownedIngredients.filter(e => e !== missingIngredient)
+  }
+
+  delIngredient(ownedIngredient) {
+    /* console.log('delIngredient works', ownedIngredient); */
+    this.ownedIngredients.push(ownedIngredient)
+    this.missingIngredients = this.missingIngredients.filter(e => e !== ownedIngredient)
+
+  }
 }
