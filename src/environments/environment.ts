68398435086f4c75c36f0102b657df6b068ea952
victoriavermo <victoriavermo@gmail.com>
2021-05-27 14:12:20 +0200

    up to date

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVB0tFKiFkijRwh_UZT5fKjxALoRFRFjw",
+    authDomain: "quecomemoshoy-1.firebaseapp.com",
+    projectId: "quecomemoshoy-1",
+    storageBucket: "quecomemoshoy-1.appspot.com",
+    messagingSenderId: "239322381581",
+    appId: "1:239322381581:web:c296df4c2c78fa33b9c444",
+    measurementId: "G-BJVN014Q79"
+  }
 };
 
 /*
