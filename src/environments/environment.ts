50e61a5af10941bca06b901d5f0ed8ccb0e0cc53
AlexP953 <alexperis95@gmail.com>
2021-05-27 17:17:47 +0200

    my-environment-commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,20 @@
+// enviroment 
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDVB0tFKiFkijRwh_UZT5fKjxALoRFRFjw",
+    authDomain: "quecomemoshoy-1.firebaseapp.com",
+    projectId: "quecomemoshoy-1",
+    storageBucket: "quecomemoshoy-1.appspot.com",
+    messagingSenderId: "239322381581",
+    appId: "1:239322381581:web:c296df4c2c78fa33b9c444",
+    measurementId: "G-BJVN014Q79"
+  }
 };
 
 /*
